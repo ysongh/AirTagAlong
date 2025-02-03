3db33b5eadada684ac97f71953aa03423919b187
You Song Hou <ysongweb3@gmail.com>
2025-02-02 19:17:55 -0500

    feat: add API for reading and writing data to nodes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import pkg from 'body-parser';
+import { SecretVaultWrapper } from 'nillion-sv-wrappers';
+import { v4 as uuidv4 } from 'uuid';
+import { orgConfig } from './nillionOrgConfig.js';
 
 // Load environment variables from .env file
 dotenv.config();
@@ -14,6 +17,64 @@ app.use(cors());
 
 // Middleware for parsing JSON bodies
 app.use(json());
+
+app.get('/store', async (req, res) => {
+  try {
+    const SCHEMA_ID = '8055defe-fcb1-4011-904b-d28ae77aa9f7';
+
+    const data = [
+      {
+        name: { $allot: 'Test Song' }, // name will be encrypted to a $share
+        years_in_web3: { $allot: 8 }, // years_in_web3 will be encrypted to a $share
+        responses: [ 
+          { rating: 5, question_number: 1 },
+          { rating: 3, question_number: 2 },
+        ], // responses will be stored in plaintext
+      },
+    ];
+
+    // Create a secret vault wrapper and initialize the SecretVault collection to use
+    const collection = new SecretVaultWrapper(
+      orgConfig.nodes,
+      orgConfig.orgCredentials,
+      SCHEMA_ID
+    );
+    await collection.init();
+
+    // Write collection data to nodes encrypting the specified fields ahead of time
+    const dataWritten = await collection.writeToNodes(data);
+    console.log(
+      '👀 Data written to nodes:',
+      JSON.stringify(dataWritten, null, 2)
+    );
+
+    // Get the ids of the SecretVault records created
+    const newIds = [
+      ...new Set(dataWritten.map((item) => item.result.data.created).flat()),
+    ];
+    console.log('uploaded record ids:', newIds);
+
+    // Read all collection data from the nodes, decrypting the specified fields
+    const decryptedCollectionData = await collection.readFromNodes({});
+
+    // Log first 5 records
+    console.log(
+      'Most recent records',
+      decryptedCollectionData.slice(0, data.length)
+    );
+   
+    res.json({ 
+      id: decryptedCollectionData[0]._id,
+      name: decryptedCollectionData[0].name,
+      years_in_web3: decryptedCollectionData[0].years_in_web3.toString(),
+      responses: decryptedCollectionData[0].responses,
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
 
 app.get('/', (req, res) => res.send('It Work'));
 
