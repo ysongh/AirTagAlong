9bc5a216f53d7c49d9c747463ee4530092547a13
You Song Hou <ysongweb3@gmail.com>
2025-09-06 21:08:10 -0400

    feat: add endpoint to encrypt a string

--- server2/routes/blindfold.js
+++ server2/routes/blindfold.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { SecretKey } from "@nillion/blindfold";
+import { SecretKey, encrypt } from "@nillion/blindfold";
 
 const router = express.Router();
 
@@ -14,5 +14,20 @@ router.get('/generatingkeys', async (req, res) => {
       res.status(500).json({ error: error.message });
   }
 });
+
+router.get('/ciphertext', async (req, res) => {
+  try {
+    const cluster = {"nodes": [{}]};
+    const secretKey = await SecretKey.generate(cluster, {"store": true});
+
+    const plaintext = "abc";
+    const ciphertext = await encrypt(secretKey, plaintext);
+    res.json({ ciphertext });
+   } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ error: error.message });
+  }
+});
+
 
 export default router;
